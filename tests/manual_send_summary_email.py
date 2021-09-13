4fd1b6139c5229ba8eaa16c65c883e70ae7a062c
Tomas Lazauskas <12182911+tomaslaz@users.noreply.github.com>
2021-09-13 12:32:13 +0300

    Adding sending an email from a manual tests functionality

--- tests/manual_send_summary_email.py
+++ tests/manual_send_summary_email.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 
 from edunotice.constants import SQL_CONNECTION_STRING_DB
 from edunotice.summary import _prep_summary_email
+from edunotice.sender import send_summary_email
 
 if __name__ == "__main__":
 
@@ -16,5 +17,9 @@ if __name__ == "__main__":
 
     if success:
         print(html_content)
+
+        success, error = send_summary_email(html_content, timestamp_utc)
+
+        print(success, error)
     else:
         print(success, error)
